13a16fc260892c9cb2d589c0391cee877d111ca6
Felice Marano <felicemarano@icloud.com>
2022-10-20 11:21:26 +0200

    Initial Commit

---
+++ Tremme Engineering/Tremme_EngineeringApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tremme_EngineeringApp.swift
+//  Tremme Engineering
+//
+//  Created by Felice Marano on 20/10/22.
+//
+
+import SwiftUI
+
+@main
+struct Tremme_EngineeringApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
